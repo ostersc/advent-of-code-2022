8c8f18c661c9d85af4aa4187d55dd2c3e8daa627
ostersc <ostersc@gmail.com>
2022-12-14 20:35:07 -0500

    day 14 complete

--- src/Day14.kt
+++ src/Day14.kt
@@ -1,22 +1,24 @@
 import kotlin.math.sign
 
 enum class ContentType { Air, Rock, Sand, Hole }
-data class Point(var x: Int, var y: Int){
-    fun down(): Point{
-        return this.copy(y=y+1)
+data class Point(var x: Int, var y: Int) {
+    fun down(): Point {
+        return this.copy(y = y + 1)
     }
-    fun diagLeft(): Point{
-        return this.copy(x=x-1,y=y+1)
+
+    fun diagLeft(): Point {
+        return this.copy(x = x - 1, y = y + 1)
     }
-    fun diagRigth(): Point{
-        return this.copy(x=x+1,y=y+1)
+
+    fun diagRight(): Point {
+        return this.copy(x = x + 1, y = y + 1)
     }
 }
 
 class CaveGrid(var hole: Point = Point(500, 0), var contents: MutableMap<Point, ContentType>) {
-    var minX = contents.keys.minOf { p -> p.x }
-    var maxX = contents.keys.maxOf { p -> p.x }
-    var maxY = contents.keys.maxOf { p -> p.y }
+    var minX = 0
+    var maxX = 0
+    var maxY = 0
 
     init {
         contents.put(hole, ContentType.Hole)
@@ -24,6 +26,12 @@ class CaveGrid(var hole: Point = Point(500, 0), var contents: MutableMap<Point, 
 
     fun isInVoid(point: Point): Boolean {
         return point.x < minX || point.x > maxX || point.y > maxY || point.y < 0
+    }
+
+    fun updateExtents() {
+        minX = contents.keys.minOf { p -> p.x }
+        maxX = contents.keys.maxOf { p -> p.x }
+        maxY = contents.keys.maxOf { p -> p.y }
     }
 
     override fun toString(): String {
@@ -52,18 +60,18 @@ class CaveGrid(var hole: Point = Point(500, 0), var contents: MutableMap<Point, 
         var s = hole.copy()
 
         while (!isInVoid(s)) {
-            if(contents.getOrDefault(s.down(), ContentType.Air)==ContentType.Air){
-                s=s.down()
-            }else if(contents.getOrDefault(s.diagLeft(), ContentType.Air)==ContentType.Air){
-                s=s.diagLeft()
-            }else if (contents.getOrDefault(s.diagRigth(), ContentType.Air)==ContentType.Air){
-                s=s.diagRigth()
-            }else{
+            if (contents.getOrDefault(s.down(), ContentType.Air) == ContentType.Air) {
+                s = s.down()
+            } else if (contents.getOrDefault(s.diagLeft(), ContentType.Air) == ContentType.Air) {
+                s = s.diagLeft()
+            } else if (contents.getOrDefault(s.diagRight(), ContentType.Air) == ContentType.Air) {
+                s = s.diagRight()
+            } else {
                 break
             }
         }
 
-        if(!isInVoid(s)) {
+        if (!isInVoid(s) && s != hole) {
             contents.put(s, ContentType.Sand)
             return true
         }
@@ -73,7 +81,8 @@ class CaveGrid(var hole: Point = Point(500, 0), var contents: MutableMap<Point, 
 
     companion object {
         fun of(input: List<String>): CaveGrid {
-            val contents = mutableMapOf<Point, ContentType>()
+            val c = CaveGrid(contents = mutableMapOf<Point, ContentType>())
+
             for (line in input) {
                 val points = line.split("->")
                 points.zipWithNext().forEach {
@@ -85,21 +94,25 @@ class CaveGrid(var hole: Point = Point(500, 0), var contents: MutableMap<Point, 
                         it.second.split(",").first().trim().toInt(),
                         it.second.split(",").last().trim().toInt()
                     )
-                    contents.put(start, ContentType.Rock)
-                    contents.put(end, ContentType.Rock)
-                    val dx = (end.x - start.x).sign
-                    val dy = (end.y - start.y).sign
-
-                    var next = start.copy().apply { x += dx; y += dy }
-                    while (next != end) {
-                        contents.put(next, ContentType.Rock)
-                        next = next.copy().apply { x += dx; y += dy }
-                    }
+                    c.addRockLine(start, end)
                 }
             }
-
-            return CaveGrid(contents = contents)
+            return c
         }
+    }
+
+    fun addRockLine(start: Point, end: Point) {
+        contents.put(start, ContentType.Rock)
+        contents.put(end, ContentType.Rock)
+        val dx = (end.x - start.x).sign
+        val dy = (end.y - start.y).sign
+
+        var next = start.copy().apply { x += dx; y += dy }
+        while (next != end) {
+            contents.put(next, ContentType.Rock)
+            next = next.copy().apply { x += dx; y += dy }
+        }
+        updateExtents()
     }
 }
 
@@ -113,12 +126,21 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val cave = CaveGrid.of(input)
+        val start = Point(cave.minX - cave.maxY, cave.maxY + 2)
+        val end = Point(cave.maxX + cave.maxY, cave.maxY + 2)
+        cave.addRockLine(start, end)
+
+        while (cave.dropSand()) {
+            //println(cave)
+        }
+
+        return cave.contents.filterValues { it.equals(ContentType.Sand) }.size + 1
     }
 
     val testInput = readInput("Day14_test")
     check(part1(testInput) == 24)
-    //check(part2(testInput) == ??)
+    check(part2(testInput) == 93)
 
     val input = readInput("Day14")
 
@@ -128,5 +150,5 @@ fun main() {
 
     val part2 = part2(input)
     println(part2)
-    //check(part2==???)
+    check(part2 == 20870)
 }
