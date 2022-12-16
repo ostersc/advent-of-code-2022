3eb6b7e3400c2097cf0283f18b2dad84b7d5bcf9
ostersc <ostersc@gmail.com>
2022-12-16 00:28:35 -0500

    day 15 done

--- src/Day15.kt
+++ src/Day15.kt
@@ -38,8 +38,10 @@ fun main() {
         var count = 0
         val grid = BeaconGrid.of(input)
 
-        val minX=grid.contents.minOf { if(it.value is Sensor) it.key.x- (it.value as Sensor).distanceFromBeacon else 0}
-        val maxX=grid.contents.maxOf { if(it.value is Sensor) it.key.x+ (it.value as Sensor).distanceFromBeacon else 0}
+        val minX =
+            grid.contents.minOf { if (it.value is Sensor) it.key.x - (it.value as Sensor).distanceFromBeacon else 0 }
+        val maxX =
+            grid.contents.maxOf { if (it.value is Sensor) it.key.x + (it.value as Sensor).distanceFromBeacon else 0 }
 
         for (i in minX..maxX) {
             val l = Location(i, row)
@@ -63,13 +65,52 @@ fun main() {
         return count
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>, max:Long): Long {
+        val grid = BeaconGrid.of(input)
+
+        // generate a list of locations that are on the edge of each sensors vision
+        val locations = mutableListOf<Location>()
+        for ((l, s) in grid.contents.filter { it.value is Sensor }) {
+            val dist = (s as Sensor).distanceFromBeacon
+            for (i in l.x - dist - 1..l.x + dist + 1) {
+                var h:Int
+                if(i<=l.x){
+                     h = (l.x-dist-i-1).absoluteValue
+                }else{
+                     h = (l.x-dist+i+1).absoluteValue
+                }
+
+                //up
+                val up = Location(i, l.y + h)
+                if (up.x > 0 && up.y >= 0 && up.x <= max && up.y <= max) {
+                    locations.add(up)
+                }
+                //down
+                val down = Location(i, l.y - h)
+                if (down.x > 0 && down.y >= 0 && down.x <= max && down.y <= max) {
+                    locations.add(down)
+                }
+            }
+        }
+
+        //find the location that is beyond all sensors vision
+        var beacon = Location(0, 0)
+        for (l in locations) {
+            if (grid.contents[l] is Sensor) {
+                continue
+            } else if (grid.contents[l] is Beacon) {
+                continue
+            } else if (grid.contents.count { it.value is Sensor && l.distanceFrom(it.key) > (it.value as Sensor).distanceFromBeacon } == input.size) {
+                beacon = l
+            }
+        }
+
+        return beacon.x * 4_000_000L + beacon.y
     }
 
     val testInput = readInput("Day15_test")
     check(part1(testInput, 10) == 26)
-    check(part2(testInput) == 56000011)
+    check(part2(testInput, 20) == 56000011L)
 
     val input = readInput("Day15")
 
@@ -77,7 +118,7 @@ fun main() {
     println(part1)
     check(part1 == 5112034)
 
-    val part2 = part2(input)
+    val part2 = part2(input, 4_000_000L)
     println(part2)
-    //check(part2==?)
+    check(part2==13172087230812L)
 }
